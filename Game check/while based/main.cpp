3e0df7ba6f7bcbbd4074931b3fe3d0b1bf6b3a67
Ivan Novikov <vanu.novik@Mail.ru>
2022-05-15 20:41:17 +0300

    Changed folder name

---
+++ Game check/while based/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	int number = 0;
+	int n;
+	cout << "¬ведите число : " ;
+	cin >> n;
+	while (n <= 50)
+	{
+		if (n % 2 == 0)
+		{
+			cout << n<< endl;
+			n += 1;
+		}
+		else
+		{
+			n += 1;
+		}
+	}
+	
+		
+		
+
+
+
+}
